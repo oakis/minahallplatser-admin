df06035af19349514ec93b4192f24f0ec8f2f1e0
Anders Ekman <oakismen@gmail.com>
2018-02-22 14:40:26 +0100

    convert provider to icon

--- src/components/Users.js
+++ src/components/Users.js
@@ -30,6 +30,21 @@ class Users extends Component {
 		this.setState({ userdata: nextProps.userdata });
 	}
 
+	getProviderIcon = (provider) => {
+		if (provider === 'facebook.com') {
+			return (
+				<svg style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
+					<path fill="#3B5998" d="M5,3H19A2,2 0 0,1 21,5V19A2,2 0 0,1 19,21H5A2,2 0 0,1 3,19V5A2,2 0 0,1 5,3M18,5H15.5A3.5,3.5 0 0,0 12,8.5V11H10V14H12V21H15V14H18V11H15V9A1,1 0 0,1 16,8H18V5Z" />
+				</svg>
+			);
+		} else if (provider === 'password') {
+			return <Icon>email</Icon>;
+		} else if (provider === 'Anonymous') {
+			return <Icon>person_outline</Icon>;
+		}
+		return '-';
+	}
+
 	handleChange = (id) => {
 		const { selected } = this.state;
 		const selectedIndex = selected.indexOf(id);
@@ -177,7 +192,7 @@ class Users extends Component {
 										{email || '-'}
 									</TableCell>
 									<TableCell>
-										{provider || '-'}
+										{this.getProviderIcon(provider)}
 									</TableCell>
 									<TableCell>
 										{lastLogin || '-'}
