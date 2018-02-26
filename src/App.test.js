8bd9d40acdd8c938ebb9c8fd6f478690844d2ba0
Anders Ekman <oakismen@gmail.com>
2018-02-26 11:26:05 +0100

    add eslint-plugin-jest, fix linting errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import App from './App';
 import renderer from 'react-test-renderer';
+import App from './App';
 
 test('App renders', () => {
-  const component = renderer.create(
-    <App />,
-  );
+  const component = renderer.create(<App />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
