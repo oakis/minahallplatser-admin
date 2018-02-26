5357ed33399b2842cdeb8ee13b551b5cd01faa55
Anders Ekman <oakismen@gmail.com>
2018-02-26 11:21:45 +0100

    added first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('App renders', () => {
+  const component = renderer.create(
+    <App />,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
